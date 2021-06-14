d2f5a30b99fe9b2e8e157c70b331a057b87c1b24
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-06-13 20:13:05 -0400

    implementation of the template success

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+declare global {
+  interface Window {
+    MyNamespace: any;
+  }
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // tslint:disable-next-line: only-arrow-functions
   }
-
 }
